5843a0ac3891de2ea264730e1a19a692461d999c
Matheus de Souza <theu205@gmail.com>
2026-04-17 20:40:15 -0300

    adicionado o cron

--- src/main/java/com/matheus/bffagendadortarefas/infrastructure/client/EmailClient.java
+++ src/main/java/com/matheus/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -16,5 +16,6 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "notificacao", url = "${notificacao.url}")
 public interface EmailClient {
 
+    @PostMapping
     void enviarEmail(@RequestBody TarefasDTOResponse dto);
 }
